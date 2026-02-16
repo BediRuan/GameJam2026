e1b28aecbedeec74d27e25428b3c9130b08bcce2
BediRuan <junchenruan92@gmail.com>
2026-02-15 17:28:36 -0800

    1

--- Assets/BediScripts/SceneLoader.cs
+++ Assets/BediScripts/SceneLoader.cs
@@ -7,7 +7,7 @@ public class SceneLoader : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             LoadScene();
         }
